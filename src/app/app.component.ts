a7c7837cf5662629fdf3e95144ccd1f3c1e94897
khurramsaeed <5n8BX6dp>
2018-03-05 12:48:23 +0100

    Løst

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {TweetService} from './tweet.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app';
+  tweets: any[];
+
+  constructor(private tweetService: TweetService) {
+
+  }
+
+  ngOnInit(): void {
+    this.tweets = this.tweetService.getTweets();
+  }
+
+  onLikeSelectedUp(totalLikes: number, iLike: boolean) {
+
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-
 import { AppComponent } from './app.component';
+import { TweetComponent } from './tweet/tweet.component';
+import {LikeComponent} from './like/like.component';
+import {TweetService} from './tweet.service';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TweetComponent,
+    LikeComponent
   ],
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [TweetService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
